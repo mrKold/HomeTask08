c6ce1ec47f3f1839d905144998948194c5fea509
IvanK <mr.koldamasov@mail.ru>
2022-11-12 15:59:35 +0300

    Задайте двумерный массив. Напишите программу,  которая поменяет местами первую и последнюю строку массива.

---
+++ EX_053/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+void InputMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(-10, 11);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+} 
+
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов: ");
+string[] num = Console.ReadLine().Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] matrix = new int[n, m];
+Console.WriteLine("Begin: ");
+InputMatrix(matrix);
+Console.WriteLine("Result: ");
+ReleaseMatrix(matrix);

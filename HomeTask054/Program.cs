571fe0fa129777afee50ab3b98e3469bbe80b81e
IvanK <mr.koldamasov@mail.ru>
2022-11-13 14:48:49 +0300

    Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.

--- HomeTask054/Program.cs
+++ HomeTask054/Program.cs
@@ -25,19 +25,20 @@ void ReleaseMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; i < matrix.GetLength(1); j++)
-        {
-            int temp = matrix[0, i];
-            matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
-            matrix[matrix.GetLength(0) - 1, i] = temp;
-        }
-
-    }
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
+            for (int k = 1; k < matrix.GetLength(1); k++)
+            {
+                for (int m = 0; m < matrix.GetLength(1) - k; m++)
+                {
+                    if (matrix[i, m] < matrix[i, m + 1])
+                    {
+                        int temp = matrix[i, m];
+                        matrix[i, m] = matrix[i, m + 1];
+                        matrix[i, m + 1] = temp;
+                    }
+                }
+            }
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
@@ -50,7 +51,7 @@ string[] num = Console.ReadLine().Split(' ');
 int n = int.Parse(num[0]);
 int m = int.Parse(num[1]);
 int[,] matrix = new int[n, m];
-Console.WriteLine("Begin: ");
+Console.WriteLine("Начальный массив: ");
 InputMatrix(matrix);
-Console.WriteLine("Result: ");
+Console.WriteLine("Отсортированный массив: ");
 ReleaseMatrix(matrix);

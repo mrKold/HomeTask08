91e6499f73f2c543b088f21754b32b70a7c022a0
IvanK <mr.koldamasov@mail.ru>
2022-11-13 15:34:46 +0300

    трёхмерный массив из двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента

---
+++ HomeTask060/Program.cs
@@ -0,0 +1,48 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void InputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+void PrintMatrix(int[,,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.WriteLine(matrix[i, j, k] + " (" + i + ", " + j + ", " + k + ")");
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.Write("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[x, y, z];
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Трёхмерный массив: ");
+PrintMatrix(matrix);

7e1b530b57d791ee97a1dc52b0adefc7aae50efa
IvanK <mr.koldamasov@mail.ru>
2022-11-13 16:15:21 +0300

    вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника

---
+++ HomeTask061/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+Console.Clear();
+int factorial(int n)
+{
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+Console.Write("Введите число элементов в основании треугольника Паскаля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i, c;
+
+for (i = 0; i < n; i++)
+{
+    for (c = 0; c <= (n - i); c++)
+    {
+        Console.Write(" ");
+    }
+    for (c = 0; c <= i; c++)
+    {
+        Console.Write(" ");
+        Console.Write(factorial(i) / (factorial(c) * factorial(i - c)));
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}

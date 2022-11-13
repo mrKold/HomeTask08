899379123b74dcc1d1b2ce92f4bf7ee6e11068a5
IvanK <mr.koldamasov@mail.ru>
2022-11-13 12:42:17 +0300

    Напишите программу, которая будет находить строку с наименьшей суммой элементов.

---
+++ HomeTask054/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; i < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[0, i];
+            matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+            matrix[matrix.GetLength(0) - 1, i] = temp;
+        }
+
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов: ");
+string[] num = Console.ReadLine().Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] matrix = new int[n, m];
+Console.WriteLine("Begin: ");
+InputMatrix(matrix);
+Console.WriteLine("Result: ");
+ReleaseMatrix(matrix);
